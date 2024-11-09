22e6640a3477f0ac6813d2ba7b4f4d61ed310c5b
Christian John jiménez Calvo <christianjjc@gmail.com>
2024-11-09 13:08:12 -0500

    cierre dia

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        heartbeat: 'heartbeat 1.5s ease-in-out infinite', // Duración y tipo de animación
+      },
+      keyframes: {
+        heartbeat: {
+          '0%, 100%': { transform: 'scale(1)' }, // Tamaño normal
+          '30%': { transform: 'scale(1.15)' }, // Aumenta de tamaño
+          '60%': { transform: 'scale(0.95)' }, // Reduce ligeramente el tamaño
+        },
+      },
+    },
+  },
+  plugins: [],
+};
