700a40d807b6dbeb13a9fee7d5634e284e97573f
Christian John jiménez Calvo <christianjjc@gmail.com>
2025-02-03 14:54:53 -0500

    Section 04 - Ep 55

---
+++ src/components/pokemons/FavoritePokemon.tsx
@@ -0,0 +1,40 @@
+import type { FavoritePokemon } from '@interfaces/favorite-pokemon';
+import { createSignal, Show, type Component } from 'solid-js';
+
+interface Props {
+  pokemon: FavoritePokemon;
+}
+
+export const FavoritePokemonCard: Component<Props> = (props) => {
+  const { pokemon } = props;
+
+  const [isVisible, setIsVisible] = createSignal(true);
+
+  const imageSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+
+  const deleteFavorite = () => {
+    const favoritePokemons = JSON.parse(localStorage.getItem('favoritePokemons') ?? '[]') as FavoritePokemon[];
+    const newPokemons = favoritePokemons.filter((p: FavoritePokemon) => p.id !== pokemon.id);
+    localStorage.setItem('favoritePokemons', JSON.stringify(newPokemons));
+    setIsVisible(false);
+  };
+
+  return (
+    //
+
+    <Show when={isVisible()}>
+      <div class="flex flex-col justify-center items-center">
+        <a href={`/pokemon/${pokemon.name}`}>
+          <img src={imageSrc} alt={pokemon.name} width="96" height="96" />
+
+          <p class="capitalize">
+            #{pokemon.id} {pokemon.name}
+          </p>
+        </a>
+        <button onclick={deleteFavorite} class="text-red-400">
+          borrar
+        </button>
+      </div>
+    </Show>
+  );
+};
